eccd8aab4191012e2f421bd5c2c26b36a4cd4a87
Parsa Rostami <parsa84rostami@gmail.com>
2024-01-09 23:48:39 +0330

    menu bar css

--- frontend/src/Components/MenuBar/MenuBar.js
+++ frontend/src/Components/MenuBar/MenuBar.js
@@ -1,31 +1,36 @@
 import React from "react";
 import { CiMenuBurger } from "react-icons/ci";
 import { Link } from "react-router-dom";
+import './MenuBar.css'
 
 export default function MenuBar() {
-  return (
-  <>
-    <div className="topBar">
-        <div className="topBarRow">
-            <div className="topBarItems">
-                <CiMenuBurger/>
-                دسته بندی ها
+    return (
+        <>
+            <div className="topBar">
+                <div className="topBarRow">
+                    <div className="topBarItems">
+                        <Link>
+                            <CiMenuBurger />
+                            دسته بندی ها
+                        </Link>
+                    </div>
+                    <div className="topBarItems">
+                        <Link>
+                            فروش ویژه
+                        </Link>
+                    </div>
+                    <div className="topBarItems">
+                        <Link>
+                            راهنمای خرید
+                        </Link>
+                    </div>
+                    <div className="topBarItems">
+                        <Link>
+                            سایر ...
+                        </Link>
+                    </div>
+                </div>
             </div>
-            <div className="topBarItems">
-                <Link>
-                    فروش ویژه
-                </Link>
-            </div>
-            <div className="topBarItems">
-                <Link>
-                    راهنمای خرید
-                </Link>
-            </div>
-            <div className="topBarItems">
-                سایر ...
-            </div>
-        </div>
-    </div>
-  </>
+        </>
     );
 }
