a0e0365538bdcfc07c3279caadbe679f1a8cb78d
Parsa Rostami <parsa84rostami@gmail.com>
2024-01-09 23:08:34 +0330

    navbar responsive

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -1,29 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { CiMenuKebab, CiSearch, CiShoppingCart } from "react-icons/ci";
+import { CiMenuBurger, CiMenuKebab, CiSearch, CiShoppingCart } from "react-icons/ci";
 import './NavBar.css'
 
 export default function NavBar() {
+
   return (
     <>
       <div className="navBar">
         <div className="navBarContainer">
           <div className="navBarLogo">
             <Link to={'/'} className='navBarLogoLink'>
-              <img src="https://roti-preview.taymakz.ir/assets/images/logo.svg" className="navBarLogoImg"/>
+              <img src="https://roti-preview.taymakz.ir/assets/images/logo.svg" className="navBarLogoImg" />
             </Link>
           </div>
           <div className="navBarSearch">
             <div className="navBarSearchGroup">
-              <button><CiSearch/></button>
+              <button><CiSearch /></button>
               <input type="text" />
             </div>
           </div>
           <div className="navBarActions">
-            <button><CiMenuKebab/></button>
-            <button><CiShoppingCart/></button>
+            <button><CiMenuKebab /></button>
+            <button><CiShoppingCart /></button>
           </div>
         </div>
+
+
+        <div className="navBarContainer2">
+          <div className="navBarUp">
+            <div className="navBarActions">
+              <button><CiMenuBurger /></button>
+            </div>
+
+            <div className="navBarLogo">
+              <Link to={'/'} className='navBarLogoLink'>
+                <img src="https://roti-preview.taymakz.ir/assets/images/logo.svg" className="navBarLogoImg" />
+              </Link>
+            </div>
+
+            <div className="navBarActions">
+              <button><CiMenuKebab /></button>
+              <button><CiShoppingCart /></button>
+            </div>
+
+          </div>
+          <div className="navBarSearch">
+            <div className="navBarSearchGroup">
+              <button><CiSearch /></button>
+              <input type="text" />
+            </div>
+          </div>
+        </div>
+
       </div>
     </>
   );
