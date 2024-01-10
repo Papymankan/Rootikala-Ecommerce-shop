30ae295cc74ceeab3ae4efec0726f0255c1974ed
Parsa Rostami <parsa84rostami@gmail.com>
2024-01-10 23:10:02 +0330

    cart Slide added

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { CiMenuBurger, CiMenuKebab, CiSearch, CiShoppingCart } from "react-icons/ci";
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import './NavBar.css'
 import MenuBar from "../MenuBar/MenuBar";
 
 export default function NavBar() {
 
-  const [showMenu , setShowMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(false)
+  const [showCart, setShowCart] = useState(true)
+
 
   return (
     <>
@@ -20,12 +23,12 @@ export default function NavBar() {
           <div className="navBarSearch">
             <div className="navBarSearchGroup">
               <button><CiSearch /></button>
-              <input type="text" placeholder="جستجو کنید ..."/>
+              <input type="text" placeholder="جستجو کنید ..." />
             </div>
           </div>
           <div className="navBarActions">
             <button><CiMenuKebab /></button>
-            <button><CiShoppingCart /></button>
+            <button onClick={()=>setShowCart(true)}><CiShoppingCart /></button>
           </div>
         </div>
 
@@ -33,7 +36,7 @@ export default function NavBar() {
         <div className="navBarContainer2">
           <div className="navBarUp">
             <div className="navBarActions">
-              <button onClick={()=>setShowMenu(true)}><CiMenuBurger /></button>
+              <button onClick={() => setShowMenu(true)}><CiMenuBurger /></button>
             </div>
 
             <div className="navBarLogo">
@@ -44,20 +47,38 @@ export default function NavBar() {
 
             <div className="navBarActions">
               <button><CiMenuKebab /></button>
-              <button><CiShoppingCart /></button>
+              <button onClick={()=>setShowCart(true)}><CiShoppingCart /></button>
             </div>
 
           </div>
           <div className="navBarSearch">
             <div className="navBarSearchGroup">
               <button><CiSearch /></button>
-              <input type="text" placeholder="جستجو کنید ..."/>
+              <input type="text" placeholder="جستجو کنید ..." />
             </div>
           </div>
         </div>
 
       </div>
-      <MenuBar showMenu={showMenu} setShowMenu={setShowMenu}/>
+      <MenuBar showMenu={showMenu} setShowMenu={setShowMenu} />
+
+      <Offcanvas show={showCart} onHide={setShowCart}>
+        <Offcanvas.Header closeButton className="cartSlideHeader">
+          <Offcanvas.Title>سبد خرید (5)</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body className="cartSlideBody">
+          <div className="cartSlideContainer"></div>
+          <div className="cartSlideActions">
+            <div>
+              <span>مبلغ قابل پرداخت</span>
+              <span>{(1350000).toLocaleString()} تومان</span>
+            </div>
+            <div>
+              <button>مشاهده و پرداخت</button>
+            </div>
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
     </>
   );
 }
