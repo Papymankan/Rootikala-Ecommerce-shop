32c9b7538faf8dccf2a27bd4953b634f78bca295
Parsa Rostami <parsa84rostami@gmail.com>
2024-01-12 21:04:16 +0330

    swiper added

--- frontend/src/Components/Landing/Landing.js
+++ frontend/src/Components/Landing/Landing.js
@@ -1,12 +1,38 @@
 import React from "react";
 import './Landing.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination , Autoplay} from 'swiper/modules';
 
 export default function Landing() {
     return (
         <>
             <div className="LandingContainer">
                 <div className="landingSwiper">
-                        <img src="/Images/main-banner-top.jpg" alt="landPic" />
+
+                    <Swiper
+                        pagination={{
+                            dynamicBullets: true,
+                        }}
+                        modules={[Pagination , Autoplay]}
+                        className="mySwiper"
+                        loop={true}
+                        autoplay={{
+                            delay: 3000,
+                            disableOnInteraction: false,
+                          }}
+                    >
+                        <SwiperSlide>
+                            <img src="/Images/main-slider-1.jpg" alt="landPic" />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="/Images/main-slider-2.jpg" alt="landPic" />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src="/Images/main-slider-3.jpg" alt="landPic" />
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
                 <div className="landingPicture">
                     <div>
