aa404d8570d49f17d21176ef895c83ba8c4ac067
Parsa Rostami <parsa84rostami@gmail.com>
2024-01-16 22:35:16 +0330

    product card structure

--- frontend/src/Components/LastProducts/LastProducts.js
+++ frontend/src/Components/LastProducts/LastProducts.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
 import './LastProducts.css'
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 
 export default function LastProducts() {
@@ -11,11 +13,42 @@ export default function LastProducts() {
                 <div className="LastCoursesContainer">
                     <div className="LastCoursesHeader">
                         <h2>جدیدترین محصولات</h2>
-                        <Link>مشاهده همه <IoIosArrowBack/></Link>
+                        <Link>مشاهده همه <IoIosArrowBack /></Link>
                     </div>
                     <div className="LastCoursesRow">
-
-                    </div>  
+                        <Swiper
+                            slidesPerView={4}
+                            spaceBetween={15}
+                            // centeredSlides={true}
+                            // pagination={{
+                            //     clickable: true,
+                            // }}
+                            // modules={[Pagination]}
+                            className="mySwiper"
+                        >
+                            <SwiperSlide className="productSlide">
+                                <div className="ProductCard">
+                                    <img src="/Images/p2.png" />
+                                    <span>تیشرت اسپورت اسپورت اسپورت اسپورت اسپورت اسپورت مردانه</span>
+                                    <span>{(1350000).toLocaleString()} تومان</span>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide className="productSlide">
+                                <div className="ProductCard">
+                                    <img src="/Images/p2.png" />
+                                    <span>تیشرت اسپورت مردانه</span>
+                                    <span>{(1350000).toLocaleString()} تومان</span>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide className="productSlide">
+                                <div className="ProductCard">
+                                    <img src="/Images/p2.png" />
+                                    <span>تیشرت اسپورت مردانه</span>
+                                    <span>{(1350000).toLocaleString()} تومان</span>
+                                </div>
+                            </SwiperSlide>
+                        </Swiper>
+                    </div>
                 </div>
             </div>
         </>
