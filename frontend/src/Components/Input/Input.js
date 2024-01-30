4f6acbd3c54ddfe959c08b580964bb8f6538a07d
Parsa Rostami <parsa84rostami@gmail.com>
2024-01-30 13:13:14 +0330

    useForm hook

--- frontend/src/Components/Input/Input.js
+++ frontend/src/Components/Input/Input.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case ('CHANGE'): {
+            console.log(state, action);
+            return { ...state, value: action.value, isValid: Validator(action.value, action.validation) }
+        }
+        case ('CLEAR'): {
+            console.log(state, action);
+            return { ...state, value: '', isValid: false }
+        }
+        default: {
+            return state
+        }
+    }
+}
 
 export default function Input(props) {
+    const [mainInput, dispatch] = useReducer(inputReducer, { value: '', isValid: false })
 
-    const element = <input placeholder={props.placeholder} type={'text'} />
+    const InputOnChange = (e) => {
+        dispatch({ type: 'CHANGE', value: e.target.value, validation: props.validation })
+    }
+
+    useEffect(() => {
+        props.onInputHandler(props.id, mainInput.value, mainInput.isValid)
+    }, [mainInput.value])
+
+    const element = <input placeholder={props.placeholder} type={'text'} onChange={InputOnChange} value={mainInput.value} className={`${mainInput.isValid ? 'success' : 'error'}`}/>
+
+
 
     return (
         <>
