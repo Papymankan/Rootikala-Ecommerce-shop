20839c0b51a8cf1d4e6340c5d0823056495cd381
Parsa Rostami <parsa84rostami@gmail.com>
2024-02-13 14:42:31 +0330

    products collection

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -7,19 +7,27 @@ import PopularProducts from "../../Components/PopularProducts/PopularProducts";
 import SaleProducts from "../../Components/SaleProducts/SaleProducts";
 
 export default function Home() {
-  const [products , setProducts] = useState([])
+  const [products, setProducts] = useState([])
 
   useEffect(() => {
     fetch(`http://localhost:9000/store/products`, {
     }).then(res => res.json()).then(data => setProducts(data.products))
+
+    fetch(`http://localhost:9000/store/products?collection_id[]=${'pcol_01HMR5RCMZ4RCE58VJ59AWXA7V'}`, {
+    }).then(res => {
+      // console.log(res)
+      return res.json()
+    }).then(data => console.log(data))
+
+    // fetch('http://localhost:9000/store/collections').then(res => res.json()).then(data => console.log(data))
   }, [])
 
   return (
     <>
       <NavBar />
       <Landing />
-      <SaleProducts products={products}/>
-      <LastProducts products={products}/>
+      <SaleProducts products={products} />
+      <LastProducts products={products} />
       <Footer />
     </>
   );
