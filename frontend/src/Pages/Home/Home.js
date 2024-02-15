800c01ee1f2dd9b421d934613b556def4ebdcd8b
Parsa Rostami <parsa84rostami@gmail.com>
2024-02-15 13:55:33 +0330

    list category Items

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -8,12 +8,15 @@ import SaleProducts from "../../Components/SaleProducts/SaleProducts";
 
 export default function Home() {
   const [products, setProducts] = useState([])
+  const [categories, setCategories] = useState([])
+  const [listedCats, setListedCats] = useState([])
 
   useEffect(() => {
     fetch(`http://localhost:9000/store/products`, {
     }).then(res => res.json()).then(data => setProducts(data.products))
+
     fetch(`http://localhost:9000/store/product-categories`, {
-    }).then(res => res.json()).then(data => console.log(data))
+    }).then(res => res.json()).then(data => setCategories(data.product_categories))
 
     // fetch(`http://localhost:9000/store/products?collection_id[]=${'pcol_01HMR5RCMZ4RCE58VJ59AWXA7V'}`, {
     // }).then(res => {
@@ -21,6 +24,26 @@ export default function Home() {
     // }).then(data => console.log(data))
 
   }, [])
+
+  useEffect(() => {
+    let arrangedCategories = categories
+    if (arrangedCategories) {
+      let arr2 = []
+      arrangedCategories.map(category => {
+        if (!category.parent_category_id) {
+          let arr = []
+          category.category_children.map(child => {
+            arrangedCategories.map(cat => {
+              cat.id == child.id && arr.push(cat);
+            })
+          })
+          category['childs'] = arr
+          arr2.push(category)
+        }
+      })
+      setListedCats(arr2)
+    }
+  }, [categories])
 
   return (
     <>
