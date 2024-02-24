93233461a39bd35d74d18e8b31233466cad6e9f6
Parsa Rostami <parsa84rostami@gmail.com>
2024-02-24 14:32:45 +0330

    category items links

--- frontend/src/Components/Categories/Categories.js
+++ frontend/src/Components/Categories/Categories.js
@@ -1,43 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import './Categories.css'
 
 export default function Categories() {
+
     return (
         <>
             <div className="Container">
+                <div className="LastCoursesHeader">
+                    <h2>دسته بندی ها</h2>
+                </div>
                 <div className="CategoriesRowContainer">
-
-
-                    <Link>
+                    <Link to={'/category/pcat_01HQ2V8MXF77XDC94V1MCKY6R6'}>
                         <div className="CategoriesRow_Item">
-                            <img src="/Images/the-ian-ZCoqcrWp9GY-unsplash.jpg" alt="" />
+                            <img src={`/Images/man1.jpg`} alt="" />
+                            <img src="/Images/man2.jpg" alt="" />
                             <span>
                                 مردانه
                             </span>
                         </div>
                     </Link>
-                    <Link>
+                    <Link to={'/category/pcat_01HQ2VJGDXSNYR1H8BJG8E86NT'}>
                         <div className="CategoriesRow_Item">
-                            <img src="/Images/the-ian-ZCoqcrWp9GY-unsplash.jpg" alt="" />
+                            <img src={`/Images/woman1.jpg`} alt="" />
+                            <img src="/Images/woman2.jpg" alt="" />
                             <span>
-                                مردانه
+                                زنانه
                             </span>
                         </div>
                     </Link>
-                    <Link>
+                    <Link to={'/category/pcat_01HQ2W1WCE60W6D3GWW71NSVRW'}>
                         <div className="CategoriesRow_Item">
-                            <img src="/Images/the-ian-ZCoqcrWp9GY-unsplash.jpg" alt="" />
+                            <img src={`/Images/perf1.jpg`} alt="" />
+                            <img src="/Images/perf2.jpg" alt="" />
                             <span>
-                                مردانه
+                                عطر و ادکلن
                             </span>
                         </div>
                     </Link>
-                    <Link>
+                    <Link to={'/category/pcat_01HQ2VWWPG9RSB03BNNW8MB1CQ'}>
                         <div className="CategoriesRow_Item">
-                            <img src="/Images/the-ian-ZCoqcrWp9GY-unsplash.jpg" alt="" />
+                            <img src={`/Images/hoodie1.jpg`} alt="" />
+                            <img src="/Images/hoodie2.jpg" alt="" />
                             <span>
-                                مردانه
+                                هودی
                             </span>
                         </div>
                     </Link>
