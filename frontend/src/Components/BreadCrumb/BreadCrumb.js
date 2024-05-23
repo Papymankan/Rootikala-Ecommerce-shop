4e1bee085e5b340465fc8684e8c1fc3f3d4e338d
Parsa Rostami <parsa84rostami@gmail.com>
2024-05-23 20:19:38 +0330

    filter header & BreadCrumb css

--- frontend/src/Components/BreadCrumb/BreadCrumb.js
+++ frontend/src/Components/BreadCrumb/BreadCrumb.js
@@ -6,27 +6,6 @@ import './BreadCrumb.css'
 export default function BreadCrumb({ categoryDetails }) {
     return (
         <>
-            {/* <div className="Container" id="breadCrumb">
-                <div className="breadCrumbContainer">
-                    <Link to={'/'}>خانه</Link>
-                    <span><IoIosArrowBack /></span>
-                    {
-                        categoryDetails.parent_category && (
-                            <>
-                                <Link to={`/category/${categoryDetails.parent_category.id}`}>{categoryDetails.parent_category.name}</Link>
-                                <span><IoIosArrowBack /></span>
-                            </>
-                        )
-                    }
-                    {
-                        categoryDetails && (
-                            <>
-                                <Link to={`/category/${categoryDetails.id}`}>{categoryDetails.name}</Link>
-                            </>
-                        )
-                    }
-                </div>
-            </div> */}
               <div className="breadCrumbContainer">
                     <Link to={'/'}>خانه</Link>
                     <span><IoIosArrowBack /></span>
