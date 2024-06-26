8efd38afcbf7fd7d55acc3634cfb9cb3f9001ac2
Parsa Rostami <parsa84rostami@gmail.com>
2024-06-26 22:43:43 +0330

    product detail title structure & fetch data

--- frontend/src/Pages/Product/Product.js
+++ frontend/src/Pages/Product/Product.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../../Components/Footer/Footer";
 import NavBar from "../../Components/NavBar/NavBar";
 import { MdShare, MdCompare } from "react-icons/md";
 import { IoMdHeartEmpty } from "react-icons/io";
 
 import './Product.css'
+import { Link, useParams } from "react-router-dom";
+// prod_01HQ2XX2RNYZGD98W5YMHHZ46B
 
 export default function Product() {
+    const { id } = useParams()
+    useEffect(() => {
+        if (id) {  
+          fetch(`http://localhost:9000/store/products/${id}`, {
+          }).then(res => {
+            return res.json()
+          }).then(data => {
+            setProduct(data)
+          })
+        }
+      }, [id])
+
+      const [product , setProduct] = useState({})
+
     return (
         <>
             <NavBar />
@@ -31,7 +47,24 @@ export default function Product() {
                     </div>
                 </div>
                 <div className="ProductVariants_Container">
+                    <div className="ProductTitle_Container">
+                        <div className="ProductTags">
+                            <span><Link to={''}>کفش</Link></span>
+                            <span><Link to={''}>کفش پیاده روی</Link></span>
+                            <span><Link to={''}>کفش</Link></span>
+                        </div>
+                        <div className="ProductName">
+                            کفش پیاده روی مردانه نیو بالانس مدل Mdrftlm2
+                        </div>
+                    </div>
+                    <div className="ProductSpecs_Container">
+                        <div className="ProductSpecs">
 
+                        </div>
+                        <div className="ProductVariants">
+
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer />
