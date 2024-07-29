2f79fe06be5fea7636b33f86e500443eceb18f2d
Parsa Rostami <parsa84rostami@gmail.com>
2024-07-29 12:19:48 +0330

    dashboard main orders section

--- frontend/src/Pages/DashBoard/DashBoardMain/DashBoardMain.js
+++ frontend/src/Pages/DashBoard/DashBoardMain/DashBoardMain.js
@@ -1,6 +1,8 @@
 import React from "react";
 import './DashBoardMain.css'
-import { MdOutlineAddLocationAlt, MdOutlineManageAccounts , MdLockOpen } from "react-icons/md";
+import EntoFa from "../../../funcs/EntoFa/EntoFa";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { MdOutlineAddLocationAlt, MdOutlineManageAccounts, MdLockOpen } from "react-icons/md";
 
 
 export default function DashBoardMain() {
@@ -27,6 +29,43 @@ export default function DashBoardMain() {
             <div className="dashboard_content_account_action">
               <span><MdOutlineAddLocationAlt /></span>
               آدرس ها
+            </div>
+          </div>
+        </div>
+
+        <div className="dashboard_content_account">
+          <div className="dashboard_content_account_title">
+            <span></span>
+            وضعیت سفارش های شما
+          </div>
+          <div className="dashboard_content_account_actions">
+            <div className="dashboard_content_order_action">
+              <span><HiOutlineShoppingBag /></span>
+              <div>
+                <span>{(0).toLocaleString().EntoFa()} سفارش</span>
+                <span>فعلی</span>
+              </div>
+            </div>
+            <div className="dashboard_content_order_action">
+              <span><MdLockOpen /></span>
+              <div>
+                <span>{(0).toLocaleString().EntoFa()} سفارش</span>
+                <span>فعلی</span>
+              </div>
+            </div>
+            <div className="dashboard_content_order_action">
+              <span><MdOutlineAddLocationAlt /></span>
+              <div>
+                <span>{(0).toLocaleString().EntoFa()} سفارش</span>
+                <span>فعلی</span>
+              </div>
+            </div>
+            <div className="dashboard_content_order_action">
+              <span><MdOutlineAddLocationAlt /></span>
+              <div>
+                <span>{(0).toLocaleString().EntoFa()} سفارش</span>
+                <span>فعلی</span>
+              </div>  
             </div>
           </div>
         </div>
