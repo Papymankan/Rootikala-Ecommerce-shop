90bd852835185281fcfa0ff397ebd8cfc60abdb4
Parsa Rostami <parsa84rostami@gmail.com>
2024-07-29 10:19:56 +0330

    dashboard menu drawer

--- frontend/src/Pages/DashBoard/DashBoard.js
+++ frontend/src/Pages/DashBoard/DashBoard.js
@@ -10,13 +10,14 @@ import { HiOutlineShoppingBag } from "react-icons/hi2";
 import { RiHomeSmile2Line } from "react-icons/ri";
 import { CiMenuBurger } from "react-icons/ci";
 import { MdOutlineAddLocationAlt, MdOutlineManageAccounts } from "react-icons/md";
+import CloseIcon from '@mui/icons-material/Close';
 import { SlLogout } from "react-icons/sl";
 
 
 export default function DashBoard() {
   const authContext = useContext(AuthContext)
   const navigate = useNavigate()
-  const [drawerOpen, setDrawerOpen] = useState(true)
+  const [drawerOpen, setDrawerOpen] = useState(false)
 
 
   const toggleDrawer = (open) => (event) => {
@@ -29,33 +30,50 @@ export default function DashBoard() {
 
   const DrawerFilter = () => {
     return (
-      <div className="Container">
-        <button onClick={toggleDrawer(false)} id='filterDrawer_Close'>
-          {/* <CloseIcon /> */}
-        </button>
-        <div className="DrawerFilters">
+      <>
+        <div className="dashboard_Menu_actionList2">
           <div>
-            <h2>فیلتر ها</h2>
-            <a>حذف همه</a>
+            <span>منوی پنل کاربری</span>
+            <button onClick={toggleDrawer(false)} >
+              <CloseIcon />
+            </button>
           </div>
-          <div className="priceRange">
-            <div>محدوده قیمت</div>
-
-            <div className="priceRange_prices">
-              {/* <span>{(value[1] / 100 * 50000000).toLocaleString()}<sub>تومان</sub> </span>
-              <span>{(value[0] / 100 * 50000000).toLocaleString()}<sub>تومان</sub> </span> */}
-            </div>
-          </div>
-          <div className="FilterToggles">
-            <span>فقط کالا های موجود</span>
-
-          </div>
-          <div className="FilterToggles">
-            <span>فقط کالا های دارای تخفیف</span>
-
-          </div>
+          <ul>
+            <li className="dashboard_Menu_actionList-active">
+              <Link>
+                <RiHomeSmile2Line />
+                <span>پیشخوان</span>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <HiOutlineShoppingBag />
+                <span>سفارش ها</span>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <MdOutlineAddLocationAlt />
+                <span>آدرس</span>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <MdOutlineManageAccounts />
+                <span>اطلاعات حساب کاربری</span>
+              </Link>
+            </li>
+            <li>
+              <Link onClick={() => {
+                authContext.logout()
+              }} to={'/'}>
+                <SlLogout />
+                <span>خروج</span>
+              </Link>
+            </li>
+          </ul>
         </div>
-      </div>
+      </>
     )
   }
 
@@ -123,10 +141,9 @@ export default function DashBoard() {
               <span>{authContext.userInfos.customer && authContext.userInfos.customer.first_name + ' ' + authContext.userInfos.customer.last_name}</span>
               <span>{('09111111111111').toLocaleString().EntoFa()}</span>
             </div>
-            <button><CiMenuBurger/> منوی کاربری</button>
+            <button onClick={() => setDrawerOpen(true)}><CiMenuBurger /> منوی کاربری</button>
           </div>
         </div>
-
 
         <div className="dashboard">
           <Outlet />
